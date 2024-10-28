2d29f6e36d91d463fc95bab1c1dc46a6c86770b5
syed yunus <syedyunusbaqi2496@gmail.com>
2024-10-28 14:39:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import myImage from '../src/image/images.jpg';
+function App() {
+
+
+
+
+  return (
+
+    <div style={{backgroundColor:"red",height:"100%",width:"100%",color:"white"}} >
+{/* <div >
+<img src={myImage} alt="Description of the image" />
+  <h1>hello syed</h1>
+
+</div> */}
+<div style={{backgroundColor:"grey", color:"white", justifyContent:"center", textAlign:"center", padding:"2rem", }}>
+<img src={myImage} style={{borderRadius:"90px",height:"180px"}} alt="Description of the image"  />
+  <h1> syed yunus </h1>
+  <p>Frontend ReactJS Developer with a strong focus on building interactive and responsive web applications. Skilled in JavaScript, modern libraries, and frameworks like React and Redux. Passionate about clean code, user experience, and optimizing performance.</p>
+</div>
+    </div>
+
+
+
+
+
+  );
+}
+
+export default App;
---
+++ src/Newlogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Newlogo = () => {
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default Newlogo
