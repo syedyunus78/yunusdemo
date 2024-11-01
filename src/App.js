eb58232b319b3279805f7506d2b826c1e072e7d3
syed yunus <syedyunusbaqi2496@gmail.com>
2024-11-01 17:03:58 +0530

    bbbbb

--- src/App.js
+++ src/App.js
@@ -15,7 +15,14 @@ function App() {
 
 </div> */}
 <div style={{backgroundColor:"black", color:"white", justifyContent:"center", textAlign:"center", padding:"2rem", }}>
-<img src={myImage} style={{borderRadius:"90px",height:"180px"}} alt="Description of the image"  />
+<div style={{
+  background: "linear-gradient(to right, #ff7e5f, #feb47b)", // Customize your colors here
+  padding: "10px", // Adjust padding to control the border width
+  borderRadius: "50%", // Ensures the background is circular
+  display: "inline-block" // Keeps the container just around the image
+}}>
+  <img src={myImage} style={{ borderRadius: "90px", height: "180px" }} alt="Description of the image" />
+</div>bb
   <h1> Syed yunus </h1>
   
   <h2 style={{color:"green" , display:"flex", textAlign:"center",justifyContent:"center"}}>Who am i ?</h2>
