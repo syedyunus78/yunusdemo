f3cd089353ee5d5f0b9b420012f34e9363724677
syed yunus <syedyunusbaqi2496@gmail.com>
2024-11-01 16:49:07 +0530

    sc

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
 </div> */}
 <div style={{backgroundColor:"grey", color:"white", justifyContent:"center", textAlign:"center", padding:"2rem", }}>
 <img src={myImage} style={{borderRadius:"90px",height:"180px"}} alt="Description of the image"  />
-  <h1> syed yunus </h1>
-  <p>software developer</p>
+  <h1> syed yunus &nbsp; (Software developer) </h1>
+ 
   <h1 style={{color:"blue"}}>Who am i ?</h1>
   <p>Frontend ReactJS Developer with a strong focus on building interactive and responsive web applications. Skilled in JavaScript, modern libraries, and frameworks like React and Redux. Passionate about clean code, user experience, and optimizing performance.</p>
 
