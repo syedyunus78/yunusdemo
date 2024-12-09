f0e23aa1c1f3059de7d3ad3053378ef7b5444487
syedyunus78 <syedyunusbaqi2496@gmail.com>
2024-12-09 13:39:34 +0530

    newww

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import myImage from '../src/image/yunuspic1.png';
 import myImagepic from "../src/image/phases_of_e_commerce_project_management_plan_slide01.jpg"
 import Newlogo from "./Newlogo";
+import Billinvoice from "./Billinvoice";
+import { Button } from "bootstrap";
+// For React (index.js)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
 
-
+const [openbill,Setbill] = useState(false)
 
 
   return (
 
-    <div style={{backgroundColor:"red",height:"100%",width:"100%",color:"black"}} >
+    <div style={{height:"100%",width:"100%",color:"black"}} >
 {/* <div >
 <img src={myImage} alt="Description of the image" />
   <h1>hello syed</h1>
@@ -34,7 +39,12 @@ function App() {
 <img  src={myImagepic} style={{width:"100%",height:"100%"}}/>
 
 <br/>
-<Newlogo/>
+
+<div  onClick={() => Setbill(true)} >invoice bill</ div>
+{openbill ? (<Billinvoice show={openbill} onHide={() => Setbill(false)}/>) : ("")}
+
+
+
     </div>
 
 
